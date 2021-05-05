3f921fd16e89293b6167ae18f20cb019ea8bfb33
Alessandra <adami.alessandra@hotmail.com>
2021-05-05 18:48:27 +0200

    78. CWD: Building A React App 3 - search box and state

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Cardlist from './Cardlist';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots';
 
 ReactDOM.render(
-  <Cardlist robots={robots}/>,
+  <App />,
   document.getElementById('root')
 );
 
