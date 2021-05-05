272ff7b0e943f435bbd97ff338849a0c6f69039e
Alessandra <adami.alessandra@hotmail.com>
2021-05-05 06:45:39 +0200

    74. CWD: Your First React Component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
 
 ReactDOM.render(
-  <h1>Hello World!</h1>,
+  <Hello greeting={'Hello ' + 'React Ninja'}/>,
   document.getElementById('root')
 );
 
